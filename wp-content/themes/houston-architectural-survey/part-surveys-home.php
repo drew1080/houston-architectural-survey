340282a3c885cf927964ef5467c86d4b9813cd53
Andrew Cobb <cobb.andrew@gmail.com>
2013-10-11 10:16:57 -0500

    Adding WP code and hayden theme with child theme.

---
+++ wp-content/themes/houston-architectural-survey/part-surveys-home.php
@@ -0,0 +1,37 @@
+<?php $home_survey_count = intval(of_get_option('ttrust_home_survey_count')); ?>
+<?php if($home_survey_count > 0) : ?>	
+<div id="survey" class="full homeSection clearfix">
+	<div class="sectionHead">				
+	<h3><span><?php echo of_get_option('ttrust_recent_surveys_title'); ?></span></h3>
+	<p><?php echo of_get_option('ttrust_recent_surveys_description'); ?></p>	
+	</div>	
+	<?php
+	if(of_get_option('ttrust_home_survey_type') == "featured") : //Show only featured surveys 
+		$args = array(
+			'ignore_sticky_posts' => 1,
+			'meta_key' => '_ttrust_survey_featured',
+			'meta_value' => true,    			
+    		'posts_per_page' => $home_survey_count,
+    		'post_type' => array(				
+				'survey'					
+				)
+			);			
+	else:
+		$args = array(
+			'ignore_sticky_posts' => 1,			  			
+    		'posts_per_page' => of_get_option('ttrust_home_survey_count'),
+    		'post_type' => array(				
+				'survey'					
+				)
+		);	
+	endif;
+	?>		
+	<?php $surveys = new WP_Query( $args ); ?>				
+	<div class="thumbs clearfix">			
+		<?php  while ($surveys->have_posts()) : $surveys->the_post(); ?>		
+			<?php get_template_part( 'part-survey-thumb'); ?>
+		<?php endwhile; ?>
+				
+	</div>
+</div>
+<?php endif; ?>
