49b18b3be04bedc0975fad0c8c0c6a44b123a9ea
Andrew Cobb <cobb.andrew@gmail.com>
2013-10-24 11:27:02 -0500

    Correct js

--- wp-content/themes/houston-architectural-survey/js/has.js
+++ wp-content/themes/houston-architectural-survey/js/has.js
@@ -1,10 +1,9 @@
 (function ($) {
   $(function() {
-    // Handler for .ready() called.
-  
     $('.taxonomy-dropdown-box').change(function(){
+      var current_option = $(this).val();
       if ( current_option != 0 ) {
-        window.location.href = $(this).val();
+        window.location.href = current_option;
       }
     });
   });
