48e3112cbe0a81ecc6851e8ae55a8cb79b739ac4
Andrew Cobb <cobb.andrew@gmail.com>
2013-11-06 10:07:16 -0600

    Adding page survey links to survey page

--- wp-content/themes/houston-architectural-survey/page-survey.php
+++ wp-content/themes/houston-architectural-survey/page-survey.php
@@ -6,6 +6,11 @@ Template Name: Survey Page
 <?php if(!is_front_page()):?>
 <div id="pageHead">
 	<h1><?php the_title(); ?></h1>
+	<?php while (have_posts()) : the_post(); ?>	
+	<div class="inside survey-links">
+	<?php the_content(); ?>	
+	</div>
+	<?php endwhile; ?>	
 	<?php $page_description = get_post_meta($post->ID, "_ttrust_page_description_value", true); ?>
 	<?php if ($page_description) : ?>
 		<p><?php echo $page_description; ?></p>
